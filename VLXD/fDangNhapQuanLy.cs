b099c9891dcdf84301914f142996f5ac72fd1f2f
Traiho <55955051+ngoctraiho98@users.noreply.github.com>
2019-10-29 12:26:00 +0700

    dnql

--- VLXD/fDangNhapQuanLy.cs
+++ VLXD/fDangNhapQuanLy.cs
@@ -10,9 +10,9 @@ using System.Windows.Forms;
 
 namespace dangnhap
 {
-    public partial class Form1 : Form
+    public partial class fDanhapQuanLy : Form
     {
-        public Form1()
+        public fDanhapQuanLy()
         {
             InitializeComponent();
         }
