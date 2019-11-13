a962098c5bf6265fc3eecf96908c9c972a501020
hohoanglong <hohoanglong1002@gmail.com>
2019-11-13 18:29:07 +0700

    test login

---
+++ UnitTest/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("UnitTest")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("UnitTest")]
+[assembly: AssemblyCopyright("Copyright ©  2019")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("22fd2027-bd6e-4cfc-9038-e2c65f8756dc")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ UnitTest/Test_Login.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+using BUS;
+using DAO;
+using DTO.Entity;
+
+namespace UnitTest
+{
+    [TestClass]
+    public class Test_Login
+    {
+        TaiKhoanBUS uBUS;
+
+        [TestMethod]
+        public void Test_LoginByNhanVien()
+        {
+            uBUS = new TaiKhoanBUS();
+            // 2 là Nhân viên
+            int expected = 2;
+            int actual = uBUS.DangNhapBUS("Huy", "123");
+
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void Test_LoginByQuanLy()
+        {
+            uBUS = new TaiKhoanBUS();
+            // 1 là Quản lý
+            int expected = 1;
+            int actual = uBUS.DangNhapBUS("Long", "123");
+
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void Test_Login_Sai_ID()
+        {
+            uBUS = new TaiKhoanBUS();
+
+            int expected = -1;
+            int actual = uBUS.DangNhapBUS("LongLong", "123");
+
+            Assert.AreEqual(expected, actual);
+        }
+        [TestMethod]
+        public void Test_Login_Sai_Pass()
+        {
+            uBUS = new TaiKhoanBUS();
+
+            int expected = -2;
+            int actual = uBUS.DangNhapBUS("Long", "321");
+
+            Assert.AreEqual(expected, actual);
+        }
+    }
+}
